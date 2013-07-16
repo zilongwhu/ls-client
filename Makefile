eb98576f25729b2ccb28e0b133badc32261bc52e
zilong.whu <zilong.whu@6a9cba3b-656e-07b5-420b-6ae627c0b417>
2013-07-16 05:28:07 +0000

    update test

--- Makefile
+++ Makefile
@@ -1,4 +1,8 @@
-client: src/*.cpp include/*.h
-	g++ -g -Wall -D__CPLUSPLUS -DDEBUG_EPEX -I../lsnet/include -Iinclude src/*.cpp ../lsnet/src/*.c -o client -lpthread
+libclient: src/*.cpp include/*.h
+	g++ -g -Wall -fPIC -shared -D__CPLUSPLUS -DDEBUG_EPEX -I../lsnet/include -Iinclude src/*.cpp ../lsnet/src/*.c -o libclient.so -lpthread
+client: test/main.cpp
+	g++ -g -Wall -D__CPLUSPLUS -DDEBUG_EPEX -I../lsnet/include -Iinclude src/*.cpp test/main.cpp ../lsnet/src/*.c -o client -lpthread
+client2: test/push.cpp
+	g++ -g -Wall -D__CPLUSPLUS -DDEBUG_EPEX -I../lsnet/include -Iinclude src/*.cpp test/push.cpp ../lsnet/src/*.c -o client2 -lpthread
 clean:
 	rm client
---
+++ test/push.cpp
@@ -0,0 +1,96 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  push.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  07/15/2013 09:51:14 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Zilong.Zhu (), zilong.whu@gmail.com
+ *   Organization:  edu.whu
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+#include <strings.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include "log.h"
+#include "client.h"
+
+Client client;
+
+char req_buf[1024];
+
+void *worker(void *args)
+{
+    NetPoller poller(&client);
+
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    struct sockaddr_in addr;
+    bzero(&addr, sizeof addr);
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(7654);
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    connect(sock, (struct sockaddr *)&addr, sizeof addr);
+
+    char res_buf[sizeof req_buf];
+    while (1)
+    {
+        NetTalk talk;
+
+        talk._sock = sock;
+        talk._status = 0;
+        talk._errno = 0;
+
+        talk._req_head._magic_num = MAGIC_NUM;
+
+        talk._req_buf = req_buf;
+        talk._req_len = sizeof req_buf;
+
+        talk._res_buf = res_buf;
+        talk._res_len = sizeof res_buf;
+
+        poller.add(&talk, 1000);
+
+        NetTalk *pt;
+        int ret = poller.poll(&pt, 1, -1);
+        if (ret > 0)
+        {
+            if (pt->_status == NET_ST_DONE)
+                NOTICE("process ok, [len=%u]", pt->_res_head._body_len);
+            else
+                WARNING("process fail, status=%d, errno=%d", pt->_status, pt->_errno);
+        }
+    }
+    close(sock);
+
+    return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+    for (int i = 0; i < sizeof req_buf/sizeof req_buf[0]; ++i)
+        req_buf[i] = rand()%26 + (rand()%2 ? 'a' : 'A');
+
+    client.init();
+
+    pthread_t workers[30];
+    for (int i = 0; i < 30; ++i)
+        pthread_create(workers + i, NULL, worker, NULL);
+
+    client.run();
+
+    for (int i = 0; i < 30; ++i)
+        pthread_join(workers[i], NULL);
+    client.join();
+    return 0;
+}
