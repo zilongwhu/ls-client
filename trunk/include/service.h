1400d08868ab89004e2d23fac66ea69d295d3c14
zilong.whu@gmail.com <zilong.whu@gmail.com@6a9cba3b-656e-07b5-420b-6ae627c0b417>
2014-04-11 15:32:07 +0000

    add service

---
+++ trunk/include/service.h
@@ -0,0 +1,51 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  service.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  04/11/2014 11:10:34 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Zilong.Zhu (), zilong.whu@gmail.com
+ *        Company:  edu.whu
+ *
+ * =====================================================================================
+ */
+
+#ifndef __LS_CLIENT_SERVICE_H__
+#define __LS_CLIENT_SERVICE_H__
+
+#include "server.h"
+
+class Service
+{
+    private:
+        Service(const Service &o);
+        Service &operator =(const Service &o);
+    public:
+        Service()
+        {
+            _servers = NULL;
+            _server_num = 0;
+        }
+        ~Service()
+        {
+            if (_servers)
+            {
+                delete [] _servers;
+                _servers = NULL;
+            }
+            _server_num = 0;
+        }
+
+        int init(const char *path, const char *file);
+    private:
+        Server *_servers;
+        int _server_num;
+};
+
+#endif
